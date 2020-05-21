3f2c2b8a76e6befa5c829fbfcd13b95c998d26e4
SaiDuc <saipandian97@gmail.com>
2020-05-21 21:36:43 +0100

    minor changes

--- main.py
+++ main.py
@@ -8,18 +8,19 @@ def psi(x, R0):
 
 
 def plotHist(data, R0, nbins=10):
-    hist = plt.hist(data, nbins, density=True)
+    hist = plt.hist(data, nbins)
+    plt.clf()
 
-    # x = np.linspace(0, max(hist[1]), 1000)
-    # y = psi(x, R0)
-    # plt.plot(x, y)
+    x = np.linspace(0, max(hist[1]), 1000)
+    y = psi(x, R0)
+    plt.plot(x, y)
 
     plt.show()
 
 
 if __name__ == '__main__':
-    box = Box(20)
-    box.populate(0.7, 1000)
+    box = Box(10)
+    box.populate(0.5, 20)
 
     box.addPlane(0, 1, -1, 0)
     box.plotBox()
@@ -28,6 +29,6 @@ if __name__ == '__main__':
 
     crossSections = box.crossSections
     print(crossSections)
-    # centres = box.centres
+    centres = box.centres
 
-    plotHist(crossSections, box.sphereRadius, 20)
+    plotHist(crossSections, 0.7, 20)
